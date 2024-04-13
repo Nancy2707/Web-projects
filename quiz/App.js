2f819cd1a0b4f5bd699397ac17ece313b42138bf
Nancy Sharma <nancysharma2000a@gmail.com>
2024-04-13 16:41:02 +0530

    Add files via upload

---
+++ quiz/App.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./App.css";
+import QuizView from "./components/QuizView";
+import ScoreView from "./components/ScoreView";
+
+function App() {
+  const questions = [
+    {
+      question: "Total number of oceans in the World is",
+      answers: [{ text: "7" }, { text: "6" }, { text: "5", isCorrect: true }],
+    },
+    {
+      question: "Who is CEO of Tesla?",
+      answers: [
+        { text: "Jeff Bezos" },
+        { text: "Elon Musk", isCorrect: true },
+        { text: "Bill Gates" },
+        { text: "Tony Stark" },
+      ],
+    },
+    {
+      question: "Where is Statue of Liberty is located?",
+      answers: [
+        { text: "India" },
+        { text: "Russia" },
+        { text: "UK" },
+        { text: "USA", isCorrect: true },
+      ],
+    },
+    {
+      question: "What is the capital of Germany?",
+      answers: [
+        { text: "Paris" },
+        { text: "Berlin", isCorrect: true },
+        { text: "London" },
+        { text: "Dublin" },
+      ],
+    },
+    {
+      question: "Which one is the largest tropical rain forest in the world?",
+      answers: [
+        { text: "Amazon", isCorrect: true },
+        { text: "Bosawas" },
+        { text: "Southeast Asian Rain Forest" },
+        { text: "Daintree Rain Forest" },
+      ],
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [isQuizOver, setIsQuizOver] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerClick = (isCorrect) => {
+    // check score
+    if (isCorrect) setScore(score + 1);
+
+    const next = currentQuestion + 1;
+    if (next < questions.length) setCurrentQuestion(next);
+    else setIsQuizOver(true);
+  };
+
+  const handleResetClick = () => {
+    // fix: score not resetting
+    setScore(0);
+
+    setCurrentQuestion(0);
+    setIsQuizOver(false);
+  };
+
+  return (
+    <div className="App">
+      {isQuizOver ? (
+        <ScoreView handleResetClick={handleResetClick} score={score} />
+      ) : (
+        <QuizView
+          questions={questions}
+          currentQuestion={currentQuestion}
+          handleAnswerClick={handleAnswerClick}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
